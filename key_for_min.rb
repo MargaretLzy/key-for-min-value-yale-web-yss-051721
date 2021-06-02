9d3b4ad997c50cee364081abf20c79d9a3ddc9fa
dynamic-daemon-637b <zl3087@columbia.edu>
2021-06-02 19:51:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,11 @@ def key_for_min_value(name_hash)
   result=nil
   
 name_hash.each do |key, value|
-if min == nil
-  min = value
-  result = key
-else
+
   if min > value
     min= value
     result = key
-  end
+
 end
 end
 result
